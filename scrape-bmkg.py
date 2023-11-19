aca2cebbeca7ea84a7cddf2d26e44fcf43523aad
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-19 14:53:01 +0700

    fix: several fixes
    - auto format for 4 spaces indentation
    - reformatted date for iqair id

--- scrape-bmkg.py
+++ scrape-bmkg.py
@@ -16,54 +16,58 @@ baseUrl = "https://ibnux.github.io/BMKG-importer/"
 import requests
 import json
 
+
 # Function to get region's weather data
-def getWeatherData (kota ,regionId, inputDate):
-  # Get weather data
-  weatherUrl = baseUrl + "cuaca/" + regionId + ".json"
-  print("Getting data from " + kota)
-  if(regionId == "0") :
-    return None
-  
-  def format_id(kota, date):
-    # Remove "Kota " or "Kab. "
-    cleaned_city_name = kota.replace("Kota ", "").replace("Kab. ", "")
+def getWeatherData(kota, regionId, inputDate):
+    # Get weather data
+    weatherUrl = baseUrl + "cuaca/" + regionId + ".json"
+    print("Getting data from " + kota)
+    if regionId == "0":
+        return None
 
-    # Convert the date string to a datetime object
-    date_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    def format_id(kota, date):
+        # Remove "Kota " or "Kab. "
+        cleaned_city_name = kota.replace("Kota ", "").replace("Kab. ", "")
 
-    # Format the date as ISO 8601
-    formatted_date = date_obj.isoformat()
+        # Convert the date string to a datetime object
+        date_obj = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
 
-    # Concatenate city name and formatted date to create the ID
-    unique_id = f"{cleaned_city_name}_{formatted_date}"
-    return unique_id
-  
-  try:
-    weatherData = requests.get(weatherUrl)
-    # Check if the request was successful
-    weatherData.raise_for_status()
-    weatherData = weatherData.json()  # Parse JSON data directly
-    # If the data's jamCuaca does not contain inputDate, remove the sub data
-    weatherData = [data for data in weatherData if data["jamCuaca"].startswith(str(inputDate))]
-    # Append extra attribute to each dictionary in weatherData
-    for data in weatherData:
-        tempKota = kota.replace("Kota ", "").replace("Kab. ", "")
-        data["kota"] = tempKota.strip()
-        data["id"] = format_id(tempKota, data["jamCuaca"])
-    return weatherData
-  except requests.exceptions.HTTPError as http_err:
-    print(f"HTTP error occurred: {http_err}")
-    return None
-  except json.JSONDecodeError as json_err:
-    print(f"JSON decoding error occurred: {json_err}")
-    return None
-  except requests.exceptions.RequestException as req_err:
-    print(f"Request error occurred: {req_err}")
-    return None
-  except Exception as err:
-    print(f"An error occurred (getWeatherData): {err}")
-    return None
-  
+        # Format the date as ISO 8601
+        formatted_date = date_obj.isoformat()
+
+        # Concatenate city name and formatted date to create the ID
+        unique_id = f"{cleaned_city_name}_{formatted_date}"
+        return unique_id
+
+    try:
+        weatherData = requests.get(weatherUrl)
+        # Check if the request was successful
+        weatherData.raise_for_status()
+        weatherData = weatherData.json()  # Parse JSON data directly
+        # If the data's jamCuaca does not contain inputDate, remove the sub data
+        weatherData = [
+            data for data in weatherData if data["jamCuaca"].startswith(str(inputDate))
+        ]
+        # Append extra attribute to each dictionary in weatherData
+        for data in weatherData:
+            tempKota = kota.replace("Kota ", "").replace("Kab. ", "")
+            data["kota"] = tempKota.strip()
+            data["id"] = format_id(tempKota, data["jamCuaca"])
+        return weatherData
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        return None
+    except json.JSONDecodeError as json_err:
+        print(f"JSON decoding error occurred: {json_err}")
+        return None
+    except requests.exceptions.RequestException as req_err:
+        print(f"Request error occurred: {req_err}")
+        return None
+    except Exception as err:
+        print(f"An error occurred (getWeatherData): {err}")
+        return None
+
+
 today = dt.date.today()
 # format today's date to YYYY-MM-DD
 inputDate = today.strftime("%Y-%m-%d")
@@ -71,44 +75,53 @@ inputDate = today.strftime("%Y-%m-%d")
 try:
     # read json
     with open("json/wilayah.json", "r") as jsonFile:
-      listOfRegions = json.load(jsonFile)
-    
+        listOfRegions = json.load(jsonFile)
+
     # Keys to extract
     regionKeys = ["id", "propinsi", "kota"]
     filteredRegions = []
 
     # Iterate over the loaded JSON data
     for region in listOfRegions:
-      regionDict = {}
-      for key in regionKeys:
-          regionDict[key] = region.get(key)
-      filteredRegions.append(regionDict)
-      
+        regionDict = {}
+        for key in regionKeys:
+            regionDict[key] = region.get(key)
+        filteredRegions.append(regionDict)
+
     # ! FOR DEBUGGING: trim to only first 10 regions
     filteredRegions = filteredRegions[:10]
-      
-    # Iterate each region and call their respective API    
+
+    # Iterate each region and call their respective API
     weatherData = []
     for region in filteredRegions:
-      tempWeatherData = getWeatherData(region["kota"] ,region["id"], today)
-      if tempWeatherData:
-        weatherData.append(tempWeatherData)
-        
-    # Write to CSV  
+        tempWeatherData = getWeatherData(region["kota"], region["id"], today)
+        if tempWeatherData:
+            weatherData.append(tempWeatherData)
+
+    # Write to CSV
     date = datetime.now().strftime("%Y-%m-%d %H:%M")
-    date = datetime.strptime(date, '%Y-%m-%d %H:%M')
+    date = datetime.strptime(date, "%Y-%m-%d %H:%M")
     date = date.strftime("%Y-%m-%d")
-    csvHeader = ["id", "kota", "jamCuaca", "kodeCuaca", "cuaca", "humidity", "tempC", "tempF"]
-    
+    csvHeader = [
+        "id",
+        "kota",
+        "jamCuaca",
+        "kodeCuaca",
+        "cuaca",
+        "humidity",
+        "tempC",
+        "tempF",
+    ]
+
     with open(f"csv/raw-bmkg/{date}.csv", "w") as csvFile:
-      csvWriter = csv.DictWriter(csvFile, fieldnames=csvHeader)
-      csvWriter.writeheader()
-      iter = 0
-      for data in weatherData:
-          for subData in data:
-            iter += 1
-            csvWriter.writerow(subData)
-            
+        csvWriter = csv.DictWriter(csvFile, fieldnames=csvHeader)
+        csvWriter.writeheader()
+        iter = 0
+        for data in weatherData:
+            for subData in data:
+                iter += 1
+                csvWriter.writerow(subData)
+
     print("Written " + str(iter) + " rows to CSV")
 
 except requests.exceptions.HTTPError as http_err:
@@ -119,7 +132,3 @@ except requests.exceptions.RequestException as req_err:
     print(f"Request error occurred: {req_err}")
 except Exception as err:
     print(f"An error occurred (Write CSV): {err}")
-
-
-
-
--- scrape-iqair.py
+++ scrape-iqair.py
@@ -238,74 +238,72 @@ endpoints = [
 ]
 
 # Base URL of IQAir website
-url = 'https://www.iqair.com/id/indonesia'
+url = "https://www.iqair.com/id/indonesia"
 dataBuffer = []
 
+
 def format_id(kota, date):
-    date_obj = datetime.strptime(date, '%Y-%m-%d %H:%M')
+    date_obj = datetime.strptime(date, "%Y-%m-%d %H")
+    print(date_obj)
     formatted_date = date_obj.isoformat()
     return kota + "_" + formatted_date
 
-def scrape(endpoint, time=datetime.now().strftime("%Y-%m-%d %H:%M")):
+def scrape(endpoint, time=datetime.now().strftime("%Y-%m-%d %H")):
     response = requests.get(url + endpoint)
     print("Scraping " + url + endpoint)
     # Check if the request was successful (status code 200)
     try:
         if response.status_code == 200:
-        # Parse HTML content
-            soup = BeautifulSoup(response.text, 'html.parser')
-            title = soup.find('h1', class_='pagetitle__title').contents[0]
-            index = soup.find('p', class_='aqi-value__value').contents[0]
-            city = soup.find('a', class_="breadcrumb__item is-active").get_text()
+            # Parse HTML content
+            soup = BeautifulSoup(response.text, "html.parser")
+            title = soup.find("h1", class_="pagetitle__title").contents[0]
+            index = soup.find("p", class_="aqi-value__value").contents[0]
+            city = soup.find("a", class_="breadcrumb__item is-active").get_text()
             city = city.replace("Kota ", "").replace("Kab. ", "")
             city = city.strip()
-            table_element = soup.find('table', attrs={'_ngcontent-airvisual-web-c224': ""})
+            table_element = soup.find(
+                "table", attrs={"_ngcontent-airvisual-web-c224": ""}
+            )
             wind = "0 km/h"
             pressure = "0 mbar"
-            for td in table_element.find_all('td'):
-                if ("mbar" in td.text):
+            for td in table_element.find_all("td"):
+                if "mbar" in td.text:
                     pressure = td.text
-                if ("km/h" in td.text):
+                if "km/h" in td.text:
                     wind = td.text
-            icon_parent = soup.find('img', class_="forecast-wind_icon")
-            wind_dir = icon_parent.get('alt')
-            
+            icon_parent = soup.find("img", class_="forecast-wind_icon")
+            wind_dir = icon_parent.get("alt")
+
             # Print the extracted data
             print("=====================================")
-            print('Title:', title)
-            print('Index:', index)
-            print('City:', city)
-            print('Wind Direction:', wind_dir)
-            print('Wind Speed:', wind)
-            print('Wind Pressure:', pressure)
-            print('Last Update:', time)
+            print("Title:", title)
+            print("Index:", index)
+            print("City:", city)
+            print("Wind Direction:", wind_dir)
+            print("Wind Speed:", wind)
+            print("Wind Pressure:", pressure)
+            print("Accessed at:", time)
             print("=====================================")
 
             # append to buffer
-            dataBuffer.append({
-                'id': format_id(city, time),
-                'kota': city,
-                'iqa': index,
-                'wind_dir(deg)': wind_dir,
-                'wind_spd(km/h)': wind,
-                'pressure(mbar)': pressure,
-                'last_update': time
-            })
+            dataBuffer.append(
+                {
+                    "id": format_id(city, time),
+                    "kota": city,
+                    "iqa": index,
+                    "wind_dir(deg)": wind_dir,
+                    "wind_spd(km/h)": wind,
+                    "pressure(mbar)": pressure,
+                    "accessed": time,
+                }
+            )
 
         else:
-            print('Failed to retrieve the webpage. Returning nulls.')
-            dataBuffer.append({
-                'id': format_id(city, time),
-                'kota': city,
-                'iqa': "null",
-                'wind_dir(deg)': "null",
-                'wind_spd(km/h)': "null",
-                'pressure(mbar)': "null",
-                'last_update': time
-            })
-            
+            print("Failed to retrieve the webpage. Returning nulls.")
+
     except:
-        print('Error Occured.')
+        print("Error Occured.")
+
 
 # ! FOR TESTING PURPOSES
 endpoints = endpoints[:5]
@@ -315,21 +313,29 @@ for endpoint in endpoints:
     scrape(endpoint)
     i += 1
     print("Scraped " + str(i) + " / " + str(len(endpoints)) + " regions.")
-    
+
 # Write the extracted data to a CSV file
 j = 0
 date = datetime.now().strftime("%Y-%m-%d %H:%M")
-date = datetime.strptime(date, '%Y-%m-%d %H:%M')
+date = datetime.strptime(date, "%Y-%m-%d %H:%M")
 date = date.strftime("%Y-%m-%d-%H")
-with open(f"csv/raw-iqair/{date}.csv", mode='w') as csv_file:
-    fieldnames = ['id', 'kota', 'iqa', 'wind_dir(deg)', 'wind_spd(km/h)', 'pressure(mbar)', 'last_update']
+with open(f"csv/raw-iqair/{date}.csv", mode="w") as csv_file:
+    fieldnames = [
+        "id",
+        "kota",
+        "iqa",
+        "wind_dir(deg)",
+        "wind_spd(km/h)",
+        "pressure(mbar)",
+        "accessed",
+    ]
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     try:
         writer.writeheader()
         for data in dataBuffer:
             writer.writerow(data)
             j += 1
-        print("Successfully wrote " + str(j) + " non-null rows to CSV file.")
+        print("Successfully wrote " + str(j) + " rows to CSV file.")
     except csv.Error as e:
         print("Error writing to CSV file.")
         print(e)
